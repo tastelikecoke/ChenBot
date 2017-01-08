f3336e881a151ac11847edc6a4a57cdef2e3c7bf
Jeru Mercado <jmerc87@gmail.com>
2017-01-08 23:26:47 +0800

    Recall old functions

--- chen.py
+++ chen.py
@@ -47,6 +47,7 @@ class Resistance:
     votes = []
     novotes = []
     current = None
+    currentIdx = 0
     channel = None
     missioneers = [0.3, 0.5, 0.7, 0.1]
     missionIndex = 0
@@ -60,7 +61,7 @@ class Resistance:
     def giveRoles(self):
         for player in self.players:
             self.roles[player] = random.choice(["resistance", "spy"])
-        self.current = random.choice(self.players)
+        self.currentIdx = random.randint(0, len(self.players))
         self.state = "assign"
     def sayPlayers(self, players):
         output = ""
@@ -112,21 +113,22 @@ class Resistance:
         self.votes = []
         self.novotes = []
         self.state = "assign"
-        self.current = random.choice(self.players)
-        await client.send_message(self.channel, "Mission {0}".format(self.missionIndex) + self.current.name + " is assigning.\n use chen assign")
+        self.current = self.players[self.currentIdx]
+        self.currentIdx = (self.currentIdx + 1) % len(self.players)
+        await client.send_message(self.channel, "Mission {0}\n".format(self.missionIndex+1) + self.current.name + " is assigning.\n use chen assign")
         await client.send_message(self.channel, "you need to assign {0}".format(self.missioneerReq()))
     async def startVote(self):
         self.votes = []
         self.novotes = []
         self.state = "vote"
-        await client.send_message(self.channel, "Vote now! either chen yes or chen no")
+        await client.send_message(self.channel, "Vote now! to vote: either say chen yes or chen no")
     async def pushMission(self):
         self.votes = []
         self.novotes = []
         self.state = "mission"
         for player in self.missioned:
             if self.roles[player] == "spy":
-                await client.send_message(player, "are you success? y/n")
+                await client.send_message(player, "are you success? say either y or n")
                 async def func(yesno):
                     if yesno:
                         await self.doMission(player)
@@ -141,11 +143,14 @@ class Resistance:
         if message.content.startswith("chen start game"):
             self.channel = message.channel
             mentioned = message.mentions
+            if len(mentioned) == 0:
+                await client.send_message(self.channel, "Pls mention players")
+                return
             self.setPlayers(mentioned)
             self.giveRoles()
             for player in self.players:
                 await client.send_message(player, "You are " + self.roles[player])
-            await client.send_message(message.channel, "players:\n" + self.sayPlayers(self.players))
+            await client.send_message(self.channel, "Players:\n" + self.sayPlayers(self.players))
             await self.startAssign()
         if message.content.startswith("chen assign"):
             if self.state == "assign":
@@ -153,33 +158,32 @@ class Resistance:
                     self.missioned = message.mentions
                     for mention in message.mentions:
                         if mention not in self.players:
-                            await client.send_message(message.channel, "pls, players only")
+                            await client.send_message(self.channel, "pls, players only")
                             return
                     if len(message.mentions) != self.missioneerReq():
-                        await client.send_message(message.channel, "pls do it again honk")
+                        await client.send_message(self.channel, "pls do it again honk")
                         return
-                    await client.send_message(message.channel, "roger!")
-                    await client.send_message(message.channel, "Mission Members:\n" + self.sayPlayers(self.missioned))
+                    await client.send_message(self.channel, "roger!")
+                    await client.send_message(self.channel, "Mission Members:\n" + self.sayPlayers(self.missioned))
                     await self.startVote()
         if message.content.startswith("chen yes"):
             if self.state == "vote":
                 if self.vote(message.author):
-                    await client.send_message(message.channel, "roger!")
+                    await client.send_message(self.channel, "roger!")
                     if self.checkMission():
-                        await client.send_message(message.channel, "Mission Start!")
+                        await client.send_message(self.channel, "Mission Start!")
                         await self.pushMission()
                 else:
-                    await client.send_message(message.channel, "no")
+                    await client.send_message(self.channel, "no")
         if message.content.startswith("chen no"):
             if self.state == "vote":
                 if self.vote(message.author):
-                    await client.send_message(message.channel, "roger!")
+                    await client.send_message(self.channel, "roger!")
                     if self.checkUnMission():
-                        await client.send_message(message.channel, "Mission Vetoed...")
+                        await client.send_message(self.channel, "Mission Vetoed...")
                         await self.startAssign()
                 else:
-                    await client.send_message(message.channel, "no")
-
+                    await client.send_message(self.channel, "no")
         if message.content.startswith("chen status"):
             await client.send_message(message.channel, "Resistance game is in " + self.state + " state.")
 
@@ -187,12 +191,28 @@ class MainCommand:
     async def ask(self, message):
         if message.content.startswith("chen pls honk"):
             await client.send_message(message.channel, "honk")
+            await asyncio.sleep(10)
+            await client.send_message(message.channel, "HONK")
+        elif message.content.startswith("chen pls help"):
+            commands = ["* chen pls honk", "* chen pls help", "* chen inspire me", "* chen pls die", "* chen start game", "* chen roll <number>", "* chen pat", "* chen lewd"]
+            await client.send_message(message.channel, "Commands are:")
+            await client.send_message(message.channel, '\n'.join(commands))
+        elif message.content.startswith("chen inspire me"):
+            await client.send_message(message.channel, "”Ｄｏｎ’ｔ　ｌｅｔ　ｙｏｕｒ　ｄｒｅａｍｓ　ｂｅ　ｍｅｍｅｓ”")
+        elif message.content.startswith("chen roll"):
+            matcher = re.match("chen roll (\d+)", message.content)
+            if matcher:
+                await client.send_message(message.channel, "You get {0}.".format(random.randint(1, int(matcher.group(1))+1)))
         elif message.content.startswith("chen pls die"):
             if message.author.name == "tastelikenyan":
                 await client.send_message(message.channel, "brb going to Gensokyo")
                 await client.logout()
                 sys.exit()
             await client.send_message(message.channel, "no")
+        elif message.content.startswith("chen pat"):
+            await client.send_message(message.channel, "http://vignette4.wikia.nocookie.net/touhouanime/images/3/34/1BdaF.jpg")
+        elif message.content.startswith("chen lewd"):
+            await client.send_message(message.channel, "http://i1.kym-cdn.com/photos/images/facebook/000/348/330/af2.png")
 
 stores = Stores()
 client = discord.Client()
@@ -215,20 +235,6 @@ client.run(stores.secrets["token"])
 
 
 f = """
-    if message.content.startswith('~test~'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith("~meme~"):
-        await client.send_message(message.channel, "Ｄｏｎ’ｔ　ｌｅｔ　ｙｏｕｒ　ｄｒｅａｍｓ　ｂｅ　ｍｅｍｅｓ")
-    elif message.content.startswith("~honk~"):
-        if message.author != client.user:
-            await client.send_message(message.channel, "ｈｏｎｋ")
-            await asyncio.sleep(10)
-            await client.send_message(message.channel, "ＨＯＮＫ")
     elif message.content.startswith("~help~"):
         await client.send_message(message.channel, "Commands are: ~sleep~, ~meme~, ~honk~, ~help~, ~voltex~")
         await client.send_message(message.channel, "Please don't bully me")
@@ -251,43 +257,4 @@ f = """
         await client.send_message(message.channel, "http://vignette4.wikia.nocookie.net/touhouanime/images/3/34/1BdaF.jpg")
     elif message.content.startswith("~lewd~"):
         await client.send_message(message.channel, "http://i1.kym-cdn.com/photos/images/facebook/000/348/330/af2.png")
-    elif message.content.startswith("~mynameis~"):
-        argument = message.content.split(" ")[1]
-        stores.store(argument)
-    elif message.content.startswith("~yourrope~"):
-        await client.send_message(message.channel, stores.get())
-    elif message.content.startswith("~players~"):
-        mentioned = message.mentions
-        stores.amount = len(mentioned)
-        players = "Players\n"
-        for player in mentioned:
-            players += str(player.name) + "\n"
-        await client.send_message(message.channel, players)
-    elif message.content.startswith("~vote~"):
-        firstmention = message.mentions[0]
-        stores.store_vote(firstmention)
-        votings = "Votal\n"
-        islynch = None
-        for vote in stores.votes:
-            votings += str(vote.name) + " - " + str(stores.votes[vote]) + "\n"
-            if stores.votes[vote] >= stores.amount/2:
-                islynch = vote
-        await client.send_message(message.channel, votings)
-        if islynch is not None:
-            stores.reset()
-            await client.send_message(message.channel, str(vote.name)+" is lynched")
-            await client.send_message(message.channel, "Next day someone died!  Chen doesn't know who")
-    elif message.content.startswith("~night~"):
-        stores.reset()
-        await client.send_message(message.channel, "Next day someone died!  Chen doesn't know who")
-    elif message.content.startswith("~roll~"):
-        argument = message.content.split(" ")[1]
-        await client.send_message(message.channel, "{0}".format(random.randint(1, int(argument)+1)))
-    elif message.content.startswith("~count~"):
-        argument = message.content.split(" ")[1]
-        period = int(argument)
-        counter = await client.send_message(message.channel, "counting...")
-        for i in range(period):
-            await asyncio.sleep(1)
-            await client.edit_message(counter, str(i))
             """
