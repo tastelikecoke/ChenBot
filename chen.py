1e14e2fbc78e7bdcab254055e68f04bd2891f0d4
Jeru Mercado <jmerc87@gmail.com>
2017-01-30 13:32:34 +0800

    Update chen

--- chen.py
+++ chen.py
@@ -8,6 +8,8 @@ import sys
 import traceback
 import re
 import difflib
+import urllib
+import datetime
 from drunk import Drunk
 from resistance import Resistance
 from lexicant import Lexicant
@@ -15,9 +17,8 @@ from lexicant import Lexicant
 class Stores:
     storedChannel = None
     localization = "en"
+    prefix = "!chen "
     def accessFile(self):
-        with open("voltexes.json", "r") as voltexesFile:
-            self.courses = json.loads(voltexesFile.read())
         with open("secrets.json", "r") as secretsFile:
             self.secrets = json.loads(secretsFile.read())
         with open("asdata.json", "r") as asDataFile:
@@ -30,6 +31,10 @@ class Stores:
         self.resistance = Resistance(client.send_message)
         self.lexicant = Lexicant(client.send_message, self.wordData)
         self.killer = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(2))
+    def store(self):
+        with open("asdata.json", "w") as asDataFile:
+            asDataFile.write(json.dumps(self.asData))
+
     def localize(self, val):
         if self.localization == "jp":
             if val == "Don't let your dreams be memes":
@@ -54,37 +59,95 @@ class Stores:
 
 class MainCommand:
     async def askResistance(self, message):
-        if message.content.startswith("chen start game"):
+        chenCommand = ""
+        matcher = re.match(stores.prefix + r"(.+)", message.content)
+        if matcher:
+            chenCommand = matcher.group(1)
+
+        if chenCommand.startswith("resistance help"):
+            commands = [
+                "resistance commands are:"
+                "**{prefix}start game**",
+                "**{prefix}assign**",
+                "**{prefix}yes**",
+                "**{prefix}no**",
+                "**{prefix}success**",
+                "**{prefix}fail**",
+                "use {0} start game to start playing resistance!".format(stores.prefix)
+            ]
+            output = '\n'.join(commands).replace('{prefix}', stores.prefix)
+            await client.send_message(message.channel, output)
+
+        if chenCommand.startswith("start game"):
             await stores.resistance.begin(message.mentions, message.channel)
-        if message.content.startswith("chen assign"):
+        if chenCommand.startswith("assign"):
             await stores.resistance.beginVote(message.author, message.mentions)
-        if message.content.startswith("chen yes"):
+        if chenCommand.startswith("yes"):
             await stores.resistance.yesVote(message.author)
-        if message.content.startswith("chen no"):
+        if chenCommand.startswith("no"):
             await stores.resistance.noVote(message.author)
-        if message.content.startswith("chen success"):
+        if chenCommand.startswith("success"):
             await stores.resistance.passMission(message.author)
-        if message.content.startswith("chen fail"):
+        if chenCommand.startswith("fail"):
             await stores.resistance.failMission(message.author)
 
     async def askLexicant(self, message):
-        if message.content.startswith("chen startlex"):
-            matcher = re.match(r"chen startlex (.+)", message.content)
+        chenCommand = ""
+        matcher = re.match(stores.prefix + r"(.+)", message.content)
+        if matcher:
+            chenCommand = matcher.group(1)
+
+        if chenCommand.startswith("lex help"):
+            await client.send_message(message.channel,\
+                "commands are:\n**{0}lex help**\n**{0}lex start**\n**{0}lex**\n**{0}lex sirit**".format(
+                    stores.prefix))
+    
+        elif chenCommand.startswith("lex start"):
+            matcher = re.match(stores.prefix + r"startlex (.+)", message.content)
             if matcher:
                 await stores.lexicant.begin(matcher.group(1), message.channel)
-        if message.content.startswith("chen lex"):
-            matcher = re.match(r"chen lex (.+)", message.content)
+
+        elif chenCommand.startswith("lex"):
+            matcher = re.match(stores.prefix + r"lex (.+)", message.content)
             if matcher:
                 await stores.lexicant.append(matcher.group(1))
-        if message.content.startswith("chen seerit"):
+
+        if chenCommand.startswith("lex sirit"):
             await stores.lexicant.seerit()
+
     async def ask(self, message):
-        if message.content.startswith("chen pls honk"):
+        chenCommand = ""
+        matcher = re.match(stores.prefix + r"(.+)", message.content)
+        if matcher:
+            chenCommand = matcher.group(1)
+
+        if chenCommand.startswith("help"):
+            commands = [
+                stores.localize("Commands are:"),
+                '**{prefix}help** this command',
+                '**{prefix}resistance help** for the resistance game',
+                '**{prefix}lex help** for the lexicant game',
+                '**{prefix}honk** honk',
+                '**{prefix}stalk** to learn about users',
+                '**{prefix}remember** to learn about users',
+                '**{prefix}lang** to switch languages',
+                '**{prefix}meme** meme',
+                '**{prefix}roll** roll dice',
+                '**{prefix}pat** pat chen',
+                '**{prefix}sing** let chen sing',
+                '**{prefix}critique** critique arts',
+                '**{prefix}prompt** drawing prompts',
+            ]
+            output = '\n'.join(commands).replace('{prefix}', stores.prefix)
+            await client.send_message(message.channel, output)
+
+        elif chenCommand.startswith("honk"):
             await client.send_message(message.channel, stores.localize("honk"))
             await asyncio.sleep(10)
             await client.send_message(message.channel, stores.localize("HONK"))
-        elif message.content.startswith("chen stalk"):
-            matcher = re.match(r"chen stalk (.+)", message.content)
+
+        elif chenCommand.startswith("stalk"):
+            matcher = re.match(r"stalk (.+)", chenCommand)
             if matcher:
                 searchkey = matcher.group(1)
                 if searchkey in stores.asData:
@@ -92,62 +155,123 @@ class MainCommand:
                 else:
                     closests = difflib.get_close_matches(searchkey, stores.asData.keys())
                     if len(closests) > 0:
-                        await client.send_message(message.channel, "closest match: "+closests[0]+"\n"+stores.asData[closests[0]])
+                        await client.send_message(message.channel,\
+                            "closest match: "+closests[0]+"\n"+stores.asData[closests[0]])
                     else:
-                        await client.send_message(message.channel, "Can't find that guy honk")
-        elif message.content.startswith("chen english pls"):
-            stores.localization = "en"
-            await client.send_message(message.channel, stores.localize("roger!"))
-        elif message.content.startswith("chen japanese pls"):
-            stores.localization = "jp"
-            await client.send_message(message.channel, stores.localize("roger!"))
-        elif message.content.startswith("chen pls help"):
-            commands = [
-                stores.localize("Commands are:"),
-                "* chen pls honk",
-                "* chen pls help",
-                "* chen inspire me",
-                "* chen pls die",
-                "* chen start game",
-                "* chen roll <number>",
-                "* chen pat",
-                "* chen lewd",
-            ]
-            await client.send_message(message.channel, '\n'.join(commands))
-        elif message.content.startswith("chen inspire me"):
-            await client.send_message(message.channel, stores.localize("Don't let your dreams be memes"))
-        elif message.content.startswith("chen roll"):
-            matcher = re.match(r"chen roll (\d+)", message.content)
+                        await client.send_message(message.channel, "Can't find honk")
+            else:
+                await client.send_message(message.channel,\
+                    "Invalid command. How to use: e.g. !chen stalk username")
+        
+        elif chenCommand.startswith("remember"):
+            matcher = re.match(r"remember ([^ ]+) (.+)", chenCommand)
             if matcher:
-                await client.send_message(message.channel, stores.localize("You get {0}.").format(random.randint(1, int(matcher.group(1))+1)))
-        elif message.content.startswith("chen pls die"):
+                searchkey = matcher.group(1)
+                searchdata = matcher.group(2)
+                stores.asData[searchkey] = searchdata
+                stores.store()
+                await client.send_message(message.channel, "{0} successfully remembered".format(searchkey))
+            else:
+                await client.send_message(message.channel,\
+                    "Invalid command. How to use: e.g. !chen remember username <links>")
+
+        elif chenCommand.startswith("lang"):
+            matcher = re.match(r"lang (.+)", chenCommand)
+            if matcher:
+                stores.localization = matcher.group(1)
+                await client.send_message(message.channel, stores.localize("roger!"))
+            else:
+                await client.send_message(message.channel,\
+                    "Invalid command. How to use: e.g. !chen lang jp")
+
+        elif chenCommand.startswith("meme"):
+            await client.send_message(message.channel,\
+                stores.localize("Don't let your dreams be memes"))
+
+        elif chenCommand.startswith("roll"):
+            matcher = re.match(r"roll (\d+)", chenCommand)
+            if matcher:
+                await client.send_message(message.channel,\
+                    stores.localize("You get {0}.").format(
+                        random.randint(1, int(matcher.group(1)))))
+
+        elif chenCommand.startswith("die"):
             if message.author.name == "tastelikenyan":
                 await client.send_message(message.channel, stores.localize("brb going to Gensokyo"))
                 await client.logout()
                 sys.exit()
             await client.send_message(message.channel, stores.localize("no"))
-        elif message.content.startswith("chen celebrate"):
-            await client.send_message(message.channel, stores.localize("✨🎉🎊✨"))
-        elif message.content.startswith("chen pat"):
-            await client.send_message(message.channel, "http://vignette4.wikia.nocookie.net/touhouanime/images/3/34/1BdaF.jpg")
-        elif message.content.startswith("chen lewd"):
-            await client.send_message(message.channel, "http://orig03.deviantart.net/115a/f/2012/266/9/9/honk_by_cybeam100-d5fnxir.jpg")
-        elif message.content.startswith("chen voltex"):
-            await client.send_message(message.channel, "play {}".format(random.choice(stores.courses)["name"]))
-        elif message.content.startswith("chen sing"):
-            await client.send_message(message.channel, Drunk().markov(random.randint(10,20)))
-        elif message.content.startswith("chen pls sing"):
-            matcher = re.match(r"chen pls sing (\w+) (\w+)", message.content)
+
+        elif chenCommand.startswith("pat"):
+            await client.send_message(message.channel,\
+                "http://vignette4.wikia.nocookie.net/touhouanime/images/3/34/1BdaF.jpg")
+
+        elif chenCommand.startswith("sing"):
+            matcher = re.match(r"sing (\w+) (\w+)", chenCommand)
             if matcher:
-                await client.send_message(message.channel, Drunk().markov(random.randint(10,20), matcher.group(1), matcher.group(2)))
-        elif message.content.startswith("chen pls critique"):
+                await client.send_message(message.channel,\
+                    Drunk().markov(random.randint(10, 20), matcher.group(1), matcher.group(2)))
+            else:
+                await client.send_message(message.channel,\
+                    Drunk().markov(random.randint(10, 20)))
+
+        elif chenCommand.startswith("critique"):
             await client.send_message(message.channel, random.choice([
                 "try fixing the anatomy",
                 "study more references",
-                "there's a bit of a problem in the hands",
+                "there's a bit of a issue about the hands",
                 "the pose is a bit awkward",
+                "add more eggplants",
+                "make the pose more dynamic",
+                "the shading can be improved"
             ]))
 
+        elif chenCommand.startswith("prompt"):
+            if chenCommand.startswith("prompt daily"):
+                todayRandom = random.Random(str(datetime.date))
+            else:
+                todayRandom = random
+
+            hairness = todayRandom.choice([
+                "white-haired",
+                "blonde",
+                "black-haired",
+                "redhead",
+                "green-haired",
+                "blue-haired"
+            ])
+            age = todayRandom.choice([
+                "9",
+                "13",
+                "16", "16",
+                "18", "18", "18", "18",
+                "20", "20", "20", "20", "20",
+                "21", "21", "21", "21",
+                "25", "25", "25",
+                "29", "29",
+                "33",
+                "40",
+                "50",
+                "70"
+            ])
+            typing = todayRandom.choice([
+                "woman", "woman", "woman", "woman", "woman", "woman",
+                "trap",
+                "man", "man", "man"
+            ])
+            action = todayRandom.choice([
+                "reading a book",
+                "smiling",
+                "smiling",
+                "posing for camera",
+                "swimming",
+                "walking the dog",
+                "thinking about life",
+                "standing",
+                "sitting",
+                "doing a selfie"
+            ])
+            await client.send_message(message.channel, "{0} {1} year old {2} {3}".format(hairness, age, typing, action))
 
 client = discord.Client()
 stores = Stores(client)
